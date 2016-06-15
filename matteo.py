3c8a1cb5de6bebe5f2716cff7f097cde1e625e57
lyndonc <lyndonc@qti.qualcomm.com>
2016-06-15 00:15:08 -0400

    Abandoning on-demand search of the St. Teresa Church website, because HTML has too many irregularities. Now, MATTEO has a saved offline copy of the dictionary (.CSV) and offline search has been implemented. Also, English search enabled for both term and definitions

--- matteo.py
+++ matteo.py
@@ -1,74 +1,22 @@
-import argparse, urllib2
-from bs4 import BeautifulSoup
-import re
+# -*- coding: utf-8 -*-
+# NOTE: Above line important for Chinese-character support
 
-def remove_trailing_space(text):
-    if text[-1] == ' ':
-        return text[0:-1]
-    else:
-        return text
-
-def remove_trailing_newline(text):
-    if text[-2:] == ' \n':
-        return text[0:-2]
-    return text
-
-def is_key(elem):
-    try:
-        if '<p' in str(elem) and 'class="green1"' in str(elem):
-            return True
-        return False
-    except:
-        return False
-
-def search_query(query):
-    # Decipher underscores as spaces
-    query = query.replace('_',' ')
-    # Access website through BeautifulSoup
-    home_url = 'http://stteresa.catholic.org.hk/catechumenate/dictionary/'
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-    initial_letter = query[0].lower()
-    query_url = home_url + initial_letter + '.htm'
-    print query_url
-    request = urllib2.Request(query_url, headers=hdr)
-    soup = BeautifulSoup(urllib2.urlopen(request).read().decode('big5', 'ignore'),'html.parser')
-    # Search for matching dictionary entries
-    key_elems = soup.findAll('p',{'class' : 'green1'})[0:-1]
-    open_bracket = '\xc2\xa1]'
-    close_bracket = '\xc2\xa1^'
-    dict = {}
-    for key_elem in key_elems:
-        # Find key (English term)
-        key = remove_trailing_space(key_elem.contents[0].encode('utf8')).replace(open_bracket,'(').replace(close_bracket,')')
-        # Find value
-        # 1. Get primary definition
-        this_elem = key_elem.next_sibling
-        prim_def = this_elem.encode('utf8')
-        # 2. Get explanation(s)
-        i = 0
-        explanation = ''
-        while not is_key(this_elem):
-            if not this_elem:
-                break
-            if i == 0:
-                explanation = this_elem
-            else:
-                explanation += this_elem
-            i += 1
-            this_elem = this_elem.next_sibling
-        # 3. Add the term, definition, and explanation to the dictionary
-        dict[key] = (prim_def,remove_trailing_newline(explanation.encode('utf8')))
-
+import argparse
+        
+def search_query(dict,query):
+    # Find matches for the query in the keys
+    match_keys = [key for key,value in dict.items() if query.lower() in key.lower() or query.lower() in value.lower()]
+    if not match_keys:
+        raise Exception("Could not find a match for your query")
+    print 'Found %d matching entries in the dictionary for your query' % (len(match_keys))
     # Writing your matching dict entries to file
     with open('search_results.csv','wb') as res_f:
-        for key,value in dict.iteritems():
-            if query in key:
-                res_f.write(key + '\t' + value[0] + '\t' + value[1] + '\n')
+        res_f.write(u'English term\t中文翻譯\n'.encode('utf8'))
+        res_f.write('========================================\n')
+        for match_key in match_keys:
+            res_f.write(match_key + '\t' + dict[match_key].encode('utf-8') + '\n')
+    print 'Your results have been saved to search_results.csv'
+    
 def split_queries(text):
     return text.split(',')
 
@@ -79,6 +27,15 @@ if __name__ == "__main__":
     parser.add_argument('-q', '--query', help='Type in a comma-separated list of the requested English terms (replace spaces with underscores)')
     args = parser.parse_args()
     queries = split_queries(args.query)
+    
+    # Import dictionary
+    dict = {}
+    dict_path = r'dict\en_zh_cath_dict.csv'
+    with open(dict_path,'r') as dict_f:
+        for row in dict_f:
+            assert(len(row.split('\t')) == 2)
+            key = row.split('\t')[0]
+            dict[key] = row.split('\t')[-1].replace('\n','').decode('utf-8')
 
     for query in queries:
-        search_query(query)
+        search_query(dict,query)
